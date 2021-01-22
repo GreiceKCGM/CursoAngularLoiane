f1138b62dc22b8029ae688713dd2308e15e9ce6c
TGM - Thiago Mota <tgmspawn@gmail.com>
2021-01-21 22:50:15 -0300

    aulas iniciais

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+
+
+
+@Component( {
+    selector: 'meu-primeiro-component',
+    template: `
+        <p> Meu primeiro Component com Angular 2! </p>
+    
+    `
+
+
+
+})
+ export class   MeuPrimeiroComponent { }
