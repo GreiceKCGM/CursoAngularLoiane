708e2f8beac01305d4d3f818175c5ad2e352f9f1
Greice Kelly <greicekellybc@gmail.com>
2021-01-25 16:17:39 -0300

    aula 27 ngFor

--- projeto-diretivas/src/app/app.module.ts
+++ projeto-diretivas/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DiretivasNgifComponent } from './diretivas-ngif/diretivas-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivasNgforComponent } from './diretivas-ngfor/diretivas-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivasNgifComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivasNgforComponent,
+    DiretivaNgclassComponent
   ],
   imports: [
     BrowserModule
